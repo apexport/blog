cf4622c3c8d87125da091cbdb6b6babee87afdde
apexport <84170572+apexport@users.noreply.github.com>
2022-03-28 09:22:13 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,9 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
-      <Footer />
     </div>
   )
 }
